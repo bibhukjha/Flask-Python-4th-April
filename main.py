e8e47ac47c8557ce21f6c241bfdeb470872ba234
bibhukjha <msgbox.ubibhu@gmail.com>
2022-04-04 15:47:41 +0530

    Flask framework dorectory integration

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from flask import Flask
-
 app = Flask(__name__)
+
 @app.route("/")
-def welcom():
-    return "Welcome to my page boss"
+def welcome():
+    return "<h1>Welcome to my page</h1>"
 
 @app.route("/contact")
 def contactPage():
-    return "I know a guy who knows a guy, contact me ASAP"
+    return render_template("contact.html")
 
 @app.route("/home")
 def homePage():
-    return "Home is where the heart is, under maintenance, come back"
+    return "<h1>Home is where the heart is, under maintenance, come back</h1>"
+
 @app.route("/Exit")
 def exitPage():
-    return "See ya later, do visit"
+    return "<h1>See ya later, do visit</h1>"
 
 if __name__ == "__main__":
     app.run()
